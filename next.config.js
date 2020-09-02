acdd6cdbe311b632288ce8fd833c2a137fa14138
krismp <kris.mp7777@gmail.com>
2020-09-03 05:41:53 +0700

    Configure nextjs custom env variable

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
+    publicRuntimeConfig: {
+        // Will only be available on the server side
+        API_BASE_URL: process.env.API_BASE_URL,
+        IMAGE_SRC: process.env.IMAGE_SRC,
+    },
+});
