4112ff2bee6ac2d7dc171f06b346fe4fc9f34bcf
krismp <kris.mp7777@gmail.com>
2020-09-03 09:26:55 +0700

    Move accordion to PokemonInfo component

--- components/Alert.js
+++ components/Alert.js
@@ -1,16 +1,16 @@
 import MuiAlert from "@material-ui/lab/Alert";
 import Snackbar from "@material-ui/core/Snackbar";
 
-function Alert(props) {
+function MyAlert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
-export default function(props) {
+export default function Alert(props) {
   return (
     <Snackbar open={props.isOpen} autoHideDuration={props.autoHideDuration} onClose={props.onClose}>
-      <Alert severity={props.severity} onClose={props.onClose}>
+      <MyAlert severity={props.severity} onClose={props.onClose}>
         {props.message}
-      </Alert>
+      </MyAlert>
     </Snackbar>
   );
 }
